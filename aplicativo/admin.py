16bc940e5467d5b9ba741cf4787553acd719dd50
HeitorMafra <91634868+HeitorMafra@users.noreply.github.com>
2021-09-29 17:49:45 -0500

    Add files via upload

---
+++ aplicativo/admin.py
@@ -0,0 +1,8 @@
+from django.contrib import admin
+from aplicativo.models import Filme, Cliente, Cartaz, Recibo, Combo
+
+# Register your models here.
+
+admin.site.register(Filme)
+admin.site.register(Cliente)
+admin.site.register(Cartaz)
