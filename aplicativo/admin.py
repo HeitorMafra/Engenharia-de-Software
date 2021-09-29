d57bda220eaddf90af921c95bf5e3a000991562f
HeitorMafra <91634868+HeitorMafra@users.noreply.github.com>
2021-09-29 17:54:10 -0500

    Delete aplicativo directory

--- aplicativo/admin.py
+++
@@ -1,8 +0,0 @@
-from django.contrib import admin
-from aplicativo.models import Filme, Cliente, Cartaz, Recibo, Combo
-
-# Register your models here.
-
-admin.site.register(Filme)
-admin.site.register(Cliente)
-admin.site.register(Cartaz)
